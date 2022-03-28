496daf828c8a5d9ede59301d13086d69dd31c618
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2022-03-28 11:26:28 +0200

    front

--- src/main/resources/public/controller/analyseChoix.js
+++ src/main/resources/public/controller/analyseChoix.js
@@ -1,31 +1,44 @@
 let url = 'http://localhost:8081/';
+
 function analysechoice(choix){
     let oldtoken = localStorage.getItem('token')
-    if (oldtoken!=''){
+    if ( oldtoken!=null){
         console.log('token old',oldtoken)
-        //TODO : Supprimer donnee du token
+        deleteEnregistrement();
     }
-    ajoutAnalyse({typeAnalyse:choix})
+    ajoutAnalyse(JSON.stringify({typeAnalyse:choix}))
     localStorage.setItem("analyse",choix);
 
 }
+
+function deleteEnregistrement(){
+    $.ajax({
+        url : url + 'remove',
+        data: {'token':localStorage.getItem('token')},
+        method: 'DELETE',
+        contentType: 'application/json',
+    }).done((data)=>{
+        console.log("sucess : ",data)
+    }).fail((data) =>{
+        console.log("fail : ",data)
+    })
+}
+
 function ajoutAnalyse(dataJson){
-    console.log('ajoutAnalyse')
+    console.log(dataJson);
     $.ajax({
         url: url + 'analyse' ,
         data : dataJson,
-        method: 'POST'
+        method: 'POST',
+        contentType:'application/json'
     })
         .done((data)=> {
-            console.log(data,data.token)
+            console.log("done :");
+            console.log(data,data.token);
             localStorage.setItem("token",data.token);
             $( "#container" ).load( "page/materiel.html" );
         })
         .fail((data) => {
-            console.log('erreur : ' ,data)
-            //TODO : a supprimer
-            // $( "#container" ).load( "page/materiel.html" );
-
+            console.log('erreur : ' ,data);
         })
-
 }
